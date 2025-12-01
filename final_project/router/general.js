84b3d55008e52ef2fa0086e760f79927325e420d
Aaditi-Patole <appatole21@gmail.com>
2025-12-01 08:57:54 -0500

    updated code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,37 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+    res.send(JSON.stringify(books,null,11));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let bookByAuthor = [];
+
+  let bookKeys = Object.keys(books);
+
+  bookKeys.forEach((key) => {
+    if(books[key].author === author) {
+        bookByAuthor.push(books[key]);
+    }
+  });
+
+    if (bookByAuthor.length === 0) {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
+
+  return res.status(200).json(bookByAuthor);
 });
 
 // Get all books based on title
